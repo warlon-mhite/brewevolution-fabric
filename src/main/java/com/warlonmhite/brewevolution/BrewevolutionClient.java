5845c0803be8f12f3b680ce46bbf8ab93e20cb79
Warlon Mhite <warlon.doctrinality@simplelogin.fr>
2025-03-19 18:42:50 +0100

    Initial commit

---
+++ src/main/java/com/warlonmhite/brewevolution/BrewevolutionClient.java
@@ -0,0 +1,11 @@
+package com.warlonmhite.brewevolution;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BrewevolutionClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/warlonmhite/brewevolution/BrewevolutionDataGenerator.java
@@ -0,0 +1,11 @@
+package com.warlonmhite.brewevolution;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BrewevolutionDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
