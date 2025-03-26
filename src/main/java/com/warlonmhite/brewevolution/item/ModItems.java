27600c95380edc8ed8967ee56000df52c6fbcb5d
EldiusCooper <eldius.cooper@gmail.com>
2025-03-26 19:10:17 +0100

    Add CHEESE BROKEN

--- src/main/java/com/warlonmhite/brewevolution/item/ModItems.java
+++ src/main/java/com/warlonmhite/brewevolution/item/ModItems.java
@@ -1,19 +1,24 @@
 package com.warlonmhite.brewevolution.item;
+
 import com.warlonmhite.brewevolution.Brewevolution;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-    public static final Item HOPS_SEEDS = registerItem("hops_seeds", new Item(new Item.Settings()));
+    public static final Item CHEESE = registerItem( "cheese", new Item(new Item.Settings()));
 
-
-    private static Item registerItem(String name, Item item){
+    private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(Brewevolution.MOD_ID, name), item);
     }
 
-    public static void registerModItems() {
-    }
-}
+    public static void registerModItems(){
+        Brewevolution.LOGGER.info("Registering Mod Items for " + Brewevolution.MOD_ID);
 
+
+    }
+
+}
