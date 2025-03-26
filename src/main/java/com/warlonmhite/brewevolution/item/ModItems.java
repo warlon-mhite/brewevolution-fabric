feccc202226e1c7553332b2cbfc6bc92c95495ce
Warlon Mhite <warlon.doctrinality@simplelogin.fr>
2025-03-26 18:15:32 +0100

    Add registerModItems

--- src/main/java/com/warlonmhite/brewevolution/item/ModItems.java
+++ src/main/java/com/warlonmhite/brewevolution/item/ModItems.java
@@ -12,5 +12,8 @@ public class ModItems {
     private static Item registerItem(String name, Item item){
         return Registry.register(Registries.ITEM, Identifier.of(Brewevolution.MOD_ID, name), item);
     }
+
+    public static void registerModItems() {
+    }
 }
 
